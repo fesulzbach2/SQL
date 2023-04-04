d6ee173424d513b2a8273a18115b338a88e78a87
Eduardo Peretto <eduardo.peretto@ufrgs.br>
2023-04-04 18:43:05 -0300

    logs and new query

--- app.py
+++ app.py
@@ -3,3 +3,4 @@ import fitness_database
 db = fitness_database.QueryHandler()
 db.getExerciseDataByDate('2023-02-01')
 db.getWeightHistoryByUser('eduardoperetto')
+db.getWeightGoalAndAverageWeightInYearByUser('eduardoperetto')
--- fitness_database.py
+++ fitness_database.py
@@ -1,50 +1,48 @@
 import psycopg2
 
 def setupDBConnection():
-        #configurações
-        host = 'localhost'
-        dbname = 'postgres'
-        user = 'postgres'
-        password = 'admin'
-        sslmode = 'allow'
+    #configurações
+    host = 'localhost'
+    dbname = 'postgres'
+    user = 'postgres'
+    password = 'admin'
+    sslmode = 'allow'
 
-        #string de conexão
-        conn_string = 'host={0} user={1} dbname={2} password={3} sslmode={4}'.format(host,user,dbname,password,sslmode)
-        conn = psycopg2.connect(conn_string)
-        print('Database succesfully connected')
-        return conn.cursor()
+    #string de conexão
+    conn_string = 'host={0} user={1} dbname={2} password={3} sslmode={4}'.format(host,user,dbname,password,sslmode)
+    conn = psycopg2.connect(conn_string)
+    print('Database succesfully connected')
+    return conn.cursor()
 
 def printQueryRows(rows):
-        for row in rows:
-            rowDescription = "("
-            for attribute in row:
-                if attribute != None:
-                    rowDescription += str(attribute) + ", "
-            rowDescription += ")"
-            print(rowDescription)
+    for row in rows:
+        rowDescription = "("
+        for attribute in row:
+            if attribute != None:
+                rowDescription += str(attribute) + ", "
+        rowDescription += ")"
+        print(rowDescription)
 
 class QueryHandler:
     def __init__(self):
         self.cursor = setupDBConnection()
 
     def getExerciseDataByDate(self, date):
+        print(" - Dados de exercícios para o dia", date)
         self.cursor.execute("""
-            SELECT 
-            e.idExercicio,
-            e.duracao,
-            t.nome as tipo_exercicio,
-            t.gastoCalorico * e.duracao / 60 as gasto_calorico
+            SELECT e.idExercicio, e.duracao, t.nome as tipo_exercicio, t.gastoCalorico * e.duracao / 60 as gasto_calorico
             FROM 
-            Exercicio e 
-            JOIN TipoExercicio t ON e.idTipoExercicio = t.idTipoExercicio 
-            JOIN Treino tr ON e.idExercicio = tr.idExercicio 
-            JOIN Diario d ON tr.idDiario = d.idDiario 
-            WHERE 
-            d.data = %s;
+                Exercicio e 
+                JOIN TipoExercicio t ON e.idTipoExercicio = t.idTipoExercicio 
+                JOIN Treino tr ON e.idExercicio = tr.idExercicio 
+                JOIN Diario d ON tr.idDiario = d.idDiario 
+            WHERE d.data = %s;
         """, (date, ))
+        print("(idExercicio, duracao, tipo_exercicio, gasto_calorico)")
         printQueryRows(self.cursor.fetchall())
 
     def getWeightHistoryByUser(self, user):
+        print(" - Histórico de pesagens para o usuário", user)
         self.cursor.execute("""
             SELECT  Peso.valor, Peso.data
             FROM 
@@ -54,4 +52,21 @@ class QueryHandler:
             WHERE u.nome = %s
             ORDER BY Peso.data DESC;
         """, (user, ))
+        print("(valor, data)")
+        printQueryRows(self.cursor.fetchall())
+
+    def getWeightGoalAndAverageWeightInYearByUser(self, user):
+        print(" - Peso alvo e peso médio para o usuário", user, "durante o ano atual")
+        self.cursor.execute("""
+            SELECT Usuario.id, Objetivo.pesoAlvo, AVG(Peso.valor) as pesoMedio
+            FROM 
+                Usuario
+                JOIN Objetivo ON Usuario.idObjetivo = Objetivo.idObjetivo
+                JOIN Pesagem ON Usuario.id = Pesagem.idUsuario
+                JOIN Peso ON Pesagem.idPeso = Peso.idPeso
+            WHERE Usuario.nome = %s 
+            AND date_part('year', Peso.data) = date_part('year', CURRENT_DATE)
+            GROUP BY Usuario.id, Objetivo.pesoAlvo;
+        """, (user, ))
+        print("(idUsuario, pesoAlvo, pesoMedio)")
         printQueryRows(self.cursor.fetchall())
