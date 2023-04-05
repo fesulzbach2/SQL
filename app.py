b9131cb5776b4d4d86f867930370a9da735e8290
FernandoSulzbach <f.sulzbach02@gmail.com>
2023-04-05 00:51:31 -0300

    APP

--- app.py
+++ app.py
@@ -1,6 +1,44 @@
 import fitness_database
 
-db = fitness_database.QueryHandler()
-db.getExerciseDataByDate('2023-02-01')
-db.getWeightHistoryByUser('eduardoperetto')
-db.getWeightGoalAndAverageWeightInYearByUser('eduardoperetto')
+choose=11
+
+while(choose!='0'):
+    choose=input("""\nCONSULTAS: 
+1-ID e nome das refeições onde houve consumo de acima de 10g de proteínas.
+2-Nome de alimentos que possuem mais de 5g de gordura
+3-Alimentos que não possuem Gordura
+4-Alimentos que nao combinam
+5-Nome de alimentos que possuem todos os nutrientes cadastrados no banco de dados
+6-ID de receitas que possuem banana em sua composição
+7-Alimentos consumidos no dia 26/01/2023
+8-Registro de pesagem em order de data decrescente do usuário ESCOLHIDO
+9-Consulta id, peso alvo e o peso médio do usuário ESCOLHIDO durante o período do ano de 2023
+10-Retorna a lista de exercícios que foram realizados no dia ESCOLHIDO, juntamente com o tipo de exercício e o gasto calórico correspondente.
+
+ESCOLHA UMA CONSULTA(insira '0' para sair): """)
+
+    db = fitness_database.QueryHandler()
+
+    if choose=='1':
+        db.getOver10gProtein()
+    elif choose=='2':
+        db.getOver5gFat()
+    elif choose=='3':
+        db.getNoFat()
+    elif choose=='4':
+        db.getNoMatching()
+    elif choose=='5':
+        db.getWithAllNutrients()
+    elif choose=='6':
+        db.getBananaInComposition()
+    elif choose=='7':
+        db.getEatenIn26()
+    elif choose=='8':
+        date=input("Entre com a data desejada (YYY-MM-DD)'2023-02-01': ")
+        db.getExerciseDataByDate(date)
+    elif choose=='9':
+        user=input("Entre com o usuario desejado: ")
+        db.getWeightHistoryByUser(user)
+    elif choose=='10':
+        user=input("Entre com o usuario desejado: ")
+        db.getWeightGoalAndAverageWeightInYearByUser(user)
--- fitness_database.py
+++ fitness_database.py
@@ -3,7 +3,7 @@ import psycopg2
 def setupDBConnection():
     #configurações
     host = 'localhost'
-    dbname = 'postgres'
+    dbname = 'FITNESS'
     user = 'postgres'
     password = 'admin'
     sslmode = 'allow'
@@ -27,18 +27,80 @@ class QueryHandler:
     def __init__(self):
         self.cursor = setupDBConnection()
 
-    def getExerciseDataByDate(self, date):
-        print(" - Dados de exercícios para o dia", date)
-        self.cursor.execute("""
-            SELECT e.idExercicio, e.duracao, t.nome as tipo_exercicio, t.gastoCalorico * e.duracao / 60 as gasto_calorico
-            FROM 
-                Exercicio e 
-                JOIN TipoExercicio t ON e.idTipoExercicio = t.idTipoExercicio 
-                JOIN Treino tr ON e.idExercicio = tr.idExercicio 
-                JOIN Diario d ON tr.idDiario = d.idDiario 
-            WHERE d.data = %s;
-        """, (date, ))
-        print("(idExercicio, duracao, tipo_exercicio, gasto_calorico)")
+    def getOver10gProtein(self):
+        print("\n(idRefeicao, Refeicao)".center(20))
+        self.cursor.execute("""select idRefeicao, Refeicao.nome from 
+                            Refeicao natural join ComposicaoRefeicao 
+                            join Alimento on(ComposicaoRefeicao.idAlimento=Alimento.idAlimento)
+                            join ComposicaoAlimentar on(ComposicaoAlimentar.idAlimento=Alimento.idAlimento)
+                            join Nutriente using(idNutriente)
+                            group by idRefeicao
+                            having sum(case when Nutriente.nome = 'Proteina' then quantidade end)>10;""")
+        printQueryRows(self.cursor.fetchall())
+
+    
+    def getOver5gFat(self):
+        print("\n(Nome)".center(20))
+        self.cursor.execute("""select Alimento.nome 
+                            from Alimento join ComposicaoAlimentar on(ComposicaoAlimentar.idAlimento=Alimento.idAlimento)
+                            join Nutriente using(idNutriente)
+                            group by Alimento.nome
+                            having  sum(case when Nutriente.nome = 'Gordura' then quantidade end)>5;""")
+        printQueryRows(self.cursor.fetchall())
+
+    def getNoFat(self):
+        print("\n(idAlimento, Nome)".center(20))
+        self.cursor.execute("""SELECT a.*
+                            FROM Alimento a
+                            WHERE NOT EXISTS (
+                            SELECT *
+                            FROM ComposicaoAlimentar c
+                            JOIN Nutriente n ON c.idNutriente = n.idNutriente
+                            WHERE c.idAlimento = a.idAlimento AND n.nome = 'Gordura');""")
+        printQueryRows(self.cursor.fetchall())
+
+    def getNoMatching(self):
+        print("\n(Nome)".center(20))
+        self.cursor.execute("""select nome
+                            from Alimento ALI
+                            where idAlimento <> 2 and
+                            not exists (select nome
+                            from ComposicaoRefeicao
+                            where idAlimento = 2 and
+                            idAlimento in
+                            (select distinct idAlimento
+                            from ComposicaoRefeicao
+                            where idAlimento = ALI.idAlimento));""")
+        printQueryRows(self.cursor.fetchall())
+
+    def getWithAllNutrients(self):
+        print("\n(Nome)".center(20))
+        self.cursor.execute("""SELECT nome
+                            FROM Alimento A
+                            WHERE NOT EXISTS (SELECT idNutriente
+                            FROM Nutriente N
+                            WHERE NOT EXISTS (SELECT idNutriente
+                                            FROM ComposicaoAlimentar C
+                                            WHERE C.idNutriente = N.idNutriente
+                                            AND C.idAlimento = A.idAlimento));""")
+        printQueryRows(self.cursor.fetchall())
+
+    def getBananaInComposition(self):
+        print("\n(idReceita)".center(20))
+        self.cursor.execute("""select idReceita 
+                            from Receita natural join Ingrediente join Alimento a using(idAlimento)
+                            where a.nome='Banana';""")
+        printQueryRows(self.cursor.fetchall())
+
+    def getEatenIn26(self):
+        print("\n(Nome)".center(20))
+        self.cursor.execute("""select Alimento.nome 
+                            from Refeicao 
+                            natural join CronogramaAlimentar 
+                            natural join Diario 
+                            join ComposicaoRefeicao using(idRefeicao) 
+                            join Alimento using (idAlimento)
+                            where data='2023-01-26';""")
         printQueryRows(self.cursor.fetchall())
 
     def getWeightHistoryByUser(self, user):
@@ -53,6 +115,20 @@ class QueryHandler:
             ORDER BY Peso.data DESC;
         """, (user, ))
         print("(valor, data)")
+        printQueryRows(self.cursor.fetchall())
+
+    def getExerciseDataByDate(self, date):
+        print(" - Dados de exercícios para o dia", date)
+        self.cursor.execute("""
+            SELECT e.idExercicio, e.duracao, t.nome as tipo_exercicio, t.gastoCalorico * e.duracao / 60 as gasto_calorico
+            FROM 
+                Exercicio e 
+                JOIN TipoExercicio t ON e.idTipoExercicio = t.idTipoExercicio 
+                JOIN Treino tr ON e.idExercicio = tr.idExercicio 
+                JOIN Diario d ON tr.idDiario = d.idDiario 
+            WHERE d.data = %s;
+        """, (date, ))
+        print("(idExercicio, duracao, tipo_exercicio, gasto_calorico)")
         printQueryRows(self.cursor.fetchall())
 
     def getWeightGoalAndAverageWeightInYearByUser(self, user):
